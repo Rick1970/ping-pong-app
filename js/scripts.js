91090084bb4fd8b7beaa7bb73d701892d26a59e3
Rick Thornbrugh <rthornbrug@gmail.com>
2016-06-24 12:54:34 -0700

    display ping in place of all numbers divisible by 3.

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,31 @@
 $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
     event.preventDefault();
+    var test = document.getElementById("ping-pong");
+    var test2 = test.elements[0].value;
+    var list = "";
 
     var input = $("input#number").val();
     parseInt(input);
-    input.split();
-    for (var counter = 0; counter < input; counter ++) {
-    $("#final").append("<li>" + counter +"</li>");
+        for (var i = 0; i < test2; i ++) {
+          list += "<li>";
+          if (i % 3 === 0){
+           list += "<b>ping</b>";
+         }
+         else {
+           list += i;
+        }
+          list += "</li>";
+
+          // if (counter % === 3) {
+          //
+          //
+          //
+          //
+          // }
+    // $("#final").append("<li>" + i +"</li>");
 
     }
+     document.getElementById("results").innerHTML = list;
   });
 });
