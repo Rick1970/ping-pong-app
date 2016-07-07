d1fe4b2a8b03d818b53baeeeee0c8be0a1946e6b
Rick1970 <rthornbrug@gmail.com>
2016-07-06 17:13:30 -0700

    add js correction

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,29 @@
-// / user interface logic here.
+var list;
+var calculate = function(test2){
+  for (var i = 1; i <= test2; i ++) {
+    list += "<li>";
+    if ((i % 3 === 0) && (i % 5 === 0)){
+      list += "<i>ping-pong</i>";
+    }
+    else if ((i % 3 === 0) && (i % 5 !== 0)){
+      list += "<i>ping</i>";
+    }
+    else if ((i % 5 === 0) && (i % 3 !== 0)){
+      list += "<i>pong</i>";
+    }
+    else {
+      list += i;
+    }
+    list += "</li>";
+  }
+};
+
 $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
-    event.preventDefault();
-
-
-    // business interface logic here.
     var test = $("input#number").val();
     var test2 = parseInt(test);
-    var list = "";
-      for (var i = 1; i <= test2; i ++) {
-      list += "<li>";
-      if ((i % 3 === 0) && (i % 5 === 0)){
-        list += "<i>ping-pong</i>"
-      }
-      else if ((i % 3 === 0) && (i % 5 !== 0)){
-       list += "<i>ping</i>";
-     }
-      else if ((i % 5 === 0) && (i % 3 !== 0)){
-        list += "<i>pong</i>"
-                }
-      else {
-       list += i;
-    }
-      list += "</li>";
-  };
-        $("#results").append(list);
+    calculate(test2);
+    $("#results").append(list);
+    event.preventDefault();
   });
 });
